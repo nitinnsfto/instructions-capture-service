8facaa32bdb4d20f9273e7f866edc4d0c7dc9775
nitinnsfto <133265034+nitinnsfto@users.noreply.github.com>
2025-10-23 09:13:55 -0400

    added log statement

--- src/main/java/com/example/instructions/controller/TradeController.java
+++ src/main/java/com/example/instructions/controller/TradeController.java
@@ -4,20 +4,26 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.instructions.model.*;
+import com.example.instructions.service.KafkaListenerService;
 import com.example.instructions.service.TradeService;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
 public class TradeController {
+	  
+	Logger logger = LoggerFactory.getLogger(KafkaListenerService.class);
 	
 	@Autowired
 	TradeService tradeService;
 
 	@PostMapping("/uploadTrades")
 	public String upload(@RequestBody List<CanonicalTrade> trades) {
+		logger.info("Recived paylod with trades as: "+ trades.size());
 		return tradeService.upload(trades);
 	}
 }
